9851c99ba881c6a2f4a7715681bdfab85002f5b6
Ferrrnando <53634355+Ferrrnando@users.noreply.github.com>
2019-12-04 00:14:10 -0400

    Agregado controlador Departamento para mantenimiento

---
+++ Controllers/DepartamentoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HubHumano.Models;
+
+namespace HubHumano.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private hubhumanoEntities1 db = new hubhumanoEntities1();
+
+        // GET: Departamento
+        public ActionResult Index()
+        {
+            return View(db.departamentoes.ToList());
+        }
+
+        // GET: Departamento/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamento departamento = db.departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamento/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Departamento/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,codigo,nombre")] departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.departamentoes.Add(departamento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(departamento);
+        }
+
+        // GET: Departamento/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamento departamento = db.departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamento/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,codigo,nombre")] departamento departamento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(departamento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(departamento);
+        }
+
+        // GET: Departamento/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            departamento departamento = db.departamentoes.Find(id);
+            if (departamento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(departamento);
+        }
+
+        // POST: Departamento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            departamento departamento = db.departamentoes.Find(id);
+            db.departamentoes.Remove(departamento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
