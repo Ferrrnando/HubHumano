f559d2bf1cd2b9d485b15d9e9c83fe193326029c
Ferrrnando <53634355+Ferrrnando@users.noreply.github.com>
2019-12-04 00:54:03 -0400

    agregado controlador de cargo en mantenimientos

---
+++ Controllers/CargoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HubHumano.Models;
+
+namespace HubHumano.Controllers
+{
+    public class CargoController : Controller
+    {
+        private hubhumanoEntities1 db = new hubhumanoEntities1();
+
+        // GET: Cargo
+        public ActionResult Index()
+        {
+            return View(db.cargoes.ToList());
+        }
+
+        // GET: Cargo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cargo/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,cargo1")] cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.cargoes.Add(cargo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cargo);
+        }
+
+        // GET: Cargo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargo/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,cargo1")] cargo cargo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cargo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cargo);
+        }
+
+        // GET: Cargo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            cargo cargo = db.cargoes.Find(id);
+            if (cargo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cargo);
+        }
+
+        // POST: Cargo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            cargo cargo = db.cargoes.Find(id);
+            db.cargoes.Remove(cargo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
